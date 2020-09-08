fe2412b96bcdfbe1845bf9b9d94644c8a41acc54
gupsy017 <gupsy017@mymail.unisa.edu.au>
2020-09-08 20:02:08 +0930

    added to menuinterface.h

--- core/menuinterface.h
+++ core/menuinterface.h
@@ -1,11 +1,19 @@
 #ifndef MENUINTERFACE_H
 #define MENUINTERFACE_H
 
+#include <iostream>
+
 namespace core {
 
 class MenuInterface {
   public:
-    MenuInterface();
+    MenuInterface() = delete;
+    MenuInterface(const std::ostream& display, const std::istream& input);
+    void displayWelcome (const std::string& author, const std::string& title) const;
+    void run();
+  private:
+    std::ostream& _display;
+    std::istream& _input;
 };
 }
 
