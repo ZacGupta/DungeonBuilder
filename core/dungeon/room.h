521a46f22b3259b8df8dd086df7e71091cf997a6
gupsy017 <gupsy017@mymail.unisa.edu.au>
2020-09-09 18:21:36 +0930

    Added more to room.h and roomedge.h.

--- core/dungeon/room.h
+++ core/dungeon/room.h
@@ -9,11 +9,14 @@
 namespace core {
 namespace dungeon {
 
+class RoomEdge;
+
 class Room {
   public:
     Room() = delete;
+    Room(const Room& other) = delete;
     Room(const int id);
-    virtual ~Room() = default;
+    virtual ~Room();
 
     friend std::ostream& operator<<(std::ostream& out, const Room& room);  //operator overload (<<)
 
@@ -28,14 +31,27 @@ class Room {
     const core::creatures::AbstractCreature& creature() const;
     void setCreature(const core::creatures::AbstractCreature& newCreature);
 
+    const RoomEdge& getNorth() const;
+    const RoomEdge& getSouth() const;
+    const RoomEdge& getEast() const;
+    const RoomEdge& getNWest() const;
+
+    //Does this require a pointer or reference?
+    void setNorth(const RoomEdge* northEdge);
+    void setSouth(const RoomEdge* southEdge);
+    void setEast(const RoomEdge* eastEdge);
+    void setWest(const RoomEdge* westEdge);
+
   private:
     const int _id;
     core::items::Item _item;
     core::creatures::AbstractCreature _creature;
-
-
-    enum class Direction : unsigned {North, South, East, West}; // Make sure this is in the correct scope
+    RoomEdge* _north;
+    RoomEdge* _south;
+    RoomEdge* _east;
+    RoomEdge* _west;
 };
+enum class Direction : unsigned {North, South, East, West}; // Make sure this is in the correct scope
 }
 }
 #endif // ROOM_H
--- core/dungeon/roomedge.h
+++ core/dungeon/roomedge.h
@@ -1,12 +1,28 @@
 #ifndef ROOMEDGE_H
 #define ROOMEDGE_H
 
+#include "core/dungeon/room.h"
+#include <string>
+
 namespace core {
 namespace dungeon {
 
 class RoomEdge {
   public:
-    RoomEdge();
+    RoomEdge() = delete;
+    RoomEdge(const RoomEdge& other) = delete;
+    RoomEdge(Direction direction);
+    virtual ~RoomEdge() = default;
+
+    friend std::ostream& operator<<(std::ostream& out, const RoomEdge& roomEdge);  //operator overload (<<)
+
+    virtual const std::string description() const = 0;
+    virtual char displayCharacter() const = 0;
+    virtual bool isPassage() const = 0;
+    const Direction& direction();
+
+  private:
+    Direction _direction;
 };
 }
 }
